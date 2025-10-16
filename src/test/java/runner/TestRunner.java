2969d5f5e9d74e82f5920639753236eac0c17c0c
santhana1907 <santhana.mari19@gmail.com>
2025-10-16 16:06:54 -0500

    allure results path added in pom

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,10 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		plugin = {"pretty", "html: target/DS-Algo.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+		plugin = {"pretty", "json:target/DS-Algo.json", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
 		monochrome = false,
 		//tags = "@login",
-		features = {"src/test/resources/features/"},
+		features = {"src/test/resources/features"},
 		glue = {"stepDefinition"})
 		
 public class TestRunner extends AbstractTestNGCucumberTests {
