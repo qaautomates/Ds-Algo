324443383c8537d5729058cb5196a0755a2d20d8
santhana1907 <santhana.mari19@gmail.com>
2025-10-15 22:05:20 -0500

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -4,10 +4,10 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-		plugin = {"pretty", "html: target/yogini.html"},
+		plugin = {"pretty", "html: target/DS-Algo.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
 		monochrome = false,
 		//tags = "@login",
-		features = {"src/test/resources/features/stack.feature"},
+		features = {"src/test/resources/features/"},
 		glue = {"stepDefinition"})
 		
 public class Runner extends AbstractTestNGCucumberTests {
