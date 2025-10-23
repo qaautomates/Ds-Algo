17c9f6dd92afb78bb35980c4bceb7c36fa60d378
Thilagavathi Amulraj <Thilagavathi.Amulraj90@gmail.com>
2025-10-23 16:54:37 -0500

    commiting Driver Factory

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -10,24 +10,26 @@ import org.openqa.selenium.edge.EdgeOptions;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
+
 public class DriverFactory {
 
-	// private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+	//private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 	private static WebDriver driver;
 
 	public static WebDriver inItBrowser() {
 		String browserName = ConfigReader.getProperty("browser");
 
 		if (browserName.equalsIgnoreCase("Edge")) {
-			// EdgeOptions options = new EdgeOptions();
-			// options.addArguments("--headless");
-			driver = new EdgeDriver();
-			return new EdgeDriver();
+			EdgeOptions options = new EdgeOptions();
+			options.addArguments("--headless");
+			driver = new EdgeDriver(options);
+			// return new EdgeDriver();
 
 		} else if (browserName.equalsIgnoreCase("Chrome")) {
-			// ChromeOptions options = new ChromeOptions();
-			// options.addArguments("--headless=new");
-			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--headless=new");   
+			driver = new ChromeDriver(options);
 
 		} else if (browserName.equalsIgnoreCase("Firefox"))
 
@@ -41,21 +43,24 @@ public class DriverFactory {
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		// threadDriver.set(driver);
+		//threadDriver.set(driver);
 		return driver;
 	}
 
-	/*
-	 * public static WebDriver getDriver() { WebDriver driver = threadDriver.get();
-	 * if (driver == null) { driver = new EdgeDriver(); // Or WebDriverManager setup
-	 * driver.manage().window().maximize(); threadDriver.set(driver); } return
-	 * driver; }
-	 */
-
+	/*public static WebDriver getDriver() {
+		WebDriver driver = threadDriver.get();
+		if (driver == null) {
+            driver = new EdgeDriver(); // Or WebDriverManager setup
+            driver.manage().window().maximize();
+            threadDriver.set(driver);
+        }
+		return driver;
+	}*/
+	
 	public static void quitDriver() {
 		if (driver != null) {
 			driver.quit();
-			// threadDriver.remove();
+			//threadDriver.remove();
 		}
 	}
 }
